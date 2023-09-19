d48278839d9cdc96344c8d4f7f0024538970527a
Manjil Tamang <lamamanjil@gmail.com>
2023-09-19 23:46:04 +0545

    add greedy algorithm solutions

---
+++ 75-least-number-of-unique-integers after-k-removals.js
@@ -0,0 +1,48 @@
+/**
+Least Number of Unique Integers after K Removals
+
+Given an array of integers arr and an integer k. Find the least number of unique integers after removing exactly k elements.
+
+ 
+
+Example 1:
+
+Input: arr = [5,5,4], k = 1
+Output: 1
+Explanation: Remove the single 4, only 5 is left.
+Example 2:
+Input: arr = [4,3,1,1,3,3,2], k = 3
+Output: 2
+Explanation: Remove 4, 2 and either one of the two 1s or three 3s. 1 and 3 will be left.
+ 
+
+Constraints:
+
+1 <= arr.length <= 10^5
+1 <= arr[i] <= 10^9
+0 <= k <= arr.length
+ */
+
+var findLeastNumOfUniqueInts = function (arr, k) {
+  const counts = new Map();
+  for (const num of arr) {
+    counts.set(num, (counts.get(num) || 0) + 1);
+  }
+
+  let ordered = Array.from(counts.values());
+  ordered.sort((a, b) => b - a);
+
+  while (k > 0) {
+    let val = ordered[ordered.length - 1];
+    if (val <= k) {
+      k -= val;
+      ordered.pop();
+    } else {
+      break;
+    }
+  }
+
+  return ordered.length;
+};
+
+console.log(findLeastNumOfUniqueInts([4, 3, 1, 1, 3, 3, 2], 3));
